ec5a3d51117177e9d981ff4d57dc08700bdbbb4d
cosaph <ccottet@student.42.fr>
2024-01-22 13:39:00 +0100

    exo2

--- tp1.py
+++ tp1.py
@@ -6,7 +6,18 @@
 #    By: ensai <ensai@student.42.fr>                +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/01/22 13:31:23 by ensai             #+#    #+#              #
-#    Updated: 2024/01/22 13:31:33 by ensai            ###   ########.fr        #
+#    Updated: 2024/01/22 13:35:12 by ensai            ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
+import datetime
+import pytz
+
+def get_current_time():
+    tz = pytz.timezone('Indian/Reunion')
+    current_time = datetime.datetime.now(tz)
+    return current_time
+
+if __name__ == '__main__':
+    current_time = get_current_time()
+    print("Heure à La Réunion :", current_time)
